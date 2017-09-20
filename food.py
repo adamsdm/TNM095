060c860868c8df1a805eca4c0422a2c49d282060
Adaso <adamsdm1@gmail.com>
2017-09-20 13:22:55 +0200

    moved stuff

--- food.py
+++ food.py
@@ -8,9 +8,13 @@ PADDING = BLOCK_SIZE/10
 GREEN = (0,255,0)
 
 class Food:
-    def __init__(self, window):
+    def __init__(self, window, GRID_SIZE, BLOCK_SIZE):
         self.position = [randint(0, GRID_SIZE[0]-1), randint(0, GRID_SIZE[1]-1)]
         self.window = window
+        self.GRID_SIZE = GRID_SIZE 
+        self.BLOCK_SIZE = BLOCK_SIZE
+        self.GREEN = (0, 255, 0)
+        self.PADDING = BLOCK_SIZE/10
     
     def draw(self):
-        pygame.draw.rect(self.window, GREEN, [self.position[0]*BLOCK_SIZE + PADDING, self.position[1]*BLOCK_SIZE + PADDING, BLOCK_SIZE-2*PADDING,BLOCK_SIZE-2*PADDING])
+        pygame.draw.rect(self.window, self.GREEN, [self.position[0]*self.BLOCK_SIZE + self.PADDING, self.position[1]*self.BLOCK_SIZE + self.PADDING, self.BLOCK_SIZE-2*self.PADDING,self.BLOCK_SIZE-2*self.PADDING])
